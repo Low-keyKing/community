a1ce308593af70a2b3ca97bac622c389253f17ac
Low-keyKing <961251452@qq.com>
2020-03-17 16:53:50 +0800

    add spring devtool and test

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -1,20 +1,20 @@
 package life.majiang.community.community.controller;
 
-import life.majiang.community.community.dto.QuestionDTO;
-import life.majiang.community.community.mapper.QuestionMapper;
-import life.majiang.community.community.mapper.UserMapper;
-import life.majiang.community.community.model.Question;
-import life.majiang.community.community.model.User;
-import life.majiang.community.community.service.QuestionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+        import life.majiang.community.community.dto.QuestionDTO;
+        import life.majiang.community.community.mapper.QuestionMapper;
+        import life.majiang.community.community.mapper.UserMapper;
+        import life.majiang.community.community.model.Question;
+        import life.majiang.community.community.model.User;
+        import life.majiang.community.community.service.QuestionService;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
+        import javax.servlet.http.Cookie;
+        import javax.servlet.http.HttpServletRequest;
+        import java.util.List;
 
 @Controller
 public class IndexController {
@@ -48,6 +48,9 @@ public class IndexController {
         }
 
         List<QuestionDTO> questionList = questionService.list();
+        for (QuestionDTO questionDTO : questionList) {
+            questionDTO.setDescription("reset");
+        }
         model.addAttribute("questions", questionList);
         return "index";
     }
