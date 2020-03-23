82fa214044624f8ec9aa8ae5390a3c7143012991
Low-keyKing <961251452@qq.com>
2020-03-23 18:39:34 +0800

    add edit

--- src/main/java/life/majiang/community/community/service/QuestionService.java
+++ src/main/java/life/majiang/community/community/service/QuestionService.java
@@ -105,4 +105,17 @@ public class QuestionService {
 
         return questionDTO;
     }
+
+    public void createOrUpdate(Question question) {
+        if (question.getId() == null){
+            //创建
+            question.setGmtCreate(System.currentTimeMillis());
+            question.setGmtModified(System.currentTimeMillis());
+            questionMapper.create(question);
+        }else {
+            //更新
+            question.setGmtModified(System.currentTimeMillis());
+            questionMapper.update(question);
+        }
+    }
 }
