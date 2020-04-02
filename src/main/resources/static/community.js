7213b7eb9bafb802bce8df3f791cef38fe2e679b
Low-keyKing <961251452@qq.com>
2020-04-02 22:03:17 +0800

    add comments list

--- src/main/resources/static/community.js
+++ src/main/resources/static/community.js
@@ -4,19 +4,26 @@ function post() {
     $.ajax({
         type: "POST",
         url: "/comment",
-        contentType:'application/json',
+        contentType: 'application/json',
         data: JSON.stringify({
-            "parentId":questionId,
-            "content":content,
-            "type":1
+            "parentId": questionId,
+            "content": content,
+            "type": 1
         }),
         success: function (response) {
-            if (response.code == 200){
+            if (response.code == 200) {
                 $("#comment_section").hide();
-            }else{
-                alert(response.message)
+            } else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=c849f385536db621cf8b&redirect_uri=http://localhost:8887/callback&scope=user&state=1")
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message)
+                }
             }
-            console.log(response);
         },
         dataType: "json"
     });
